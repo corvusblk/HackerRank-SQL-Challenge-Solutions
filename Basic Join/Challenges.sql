bf5d91e0b0461ed79520445c548fbcda21fd0f64
Isaac Conrique <iconriqu@ucsd.edu>
2023-06-26 16:05:40 -0700

    completed `Basic Join` solutions

--- Basic Join/Challenges.sql
+++ Basic Join/Challenges.sql
@@ -1,8 +1,8 @@
 /* CTEs */
 WITH challenge_counts
 AS(  SELECT COUNT(hacker_id) AS count
-            FROM   challenges AS c
-            GROUP  BY hacker_id
+     FROM   challenges AS c
+     GROUP  BY hacker_id
 ),
 distinct_challenge_counts
 AS(  SELECT count
@@ -22,4 +22,4 @@ GROUP  BY c.hacker_id
 HAVING COUNT(c.hacker_id) = (SELECT MAX(count) FROM challenge_counts)
        OR COUNT(c.hacker_id) IN (SELECT * FROM distinct_challenge_counts)
 ORDER  BY COUNT(c.hacker_id) DESC,
-       c.hacker_id
+       c.hacker_id;
